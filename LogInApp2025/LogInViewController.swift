73e20c880209aeb3b4aa1a7b852e390335d6a351
Yuri Luka <lukayurii.work@gmail.com>
2024-12-04 17:52:00 +0500

    Added Alert Controllers for username and password

--- LogInApp2025/LogInViewController.swift
+++ LogInApp2025/LogInViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+    
+    @IBOutlet var welcomeMessageLabel: UILabel!
+    @IBOutlet var helloLabel: UILabel!
+    
+    let userName: String! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        helloLabel.text = "👋"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButtonTapped() {
+        
     }
-    */
-
 }
--- LogInApp2025/MainViewController.swift
+++ LogInApp2025/MainViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    //MARK: - IBOuntlets
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    //MARK: - Private properties
+    private let userName = "User1"
+    private let password = "Password1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - IBActions
+    @IBAction func logInButtonTapped() {
+        
+    }
+    
+    @IBAction func fogotUserNameButtonTapped() {
+        showAlert(title: "Oops!", message: "Your name is \(userName) 😉")
+    }
+    
+    @IBAction func fogotPasswordButtonTapped() {
+            showAlert(title: "Oops!", message: "Your name is \(password) 😉")
+    }
 }
 
+    // MARK: - Extantions
+extension MainViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
