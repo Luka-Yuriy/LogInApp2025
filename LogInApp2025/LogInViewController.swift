5839cafdca880bb865283cad02cb31faa51973c8
Yuri Luka <lukayurii.work@gmail.com>
2024-12-05 12:59:02 +0500

    added final class

--- LogInApp2025/LogInViewController.swift
+++ LogInApp2025/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LogInViewController: UIViewController {
+final class LogInViewController: UIViewController {
     //MARK: - IBOuntlets
     @IBOutlet var welcomeMessageLabel: UILabel!
     
